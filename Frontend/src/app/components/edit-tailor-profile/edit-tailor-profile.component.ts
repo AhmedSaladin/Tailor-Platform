7fb8aad2bce9b6ff01e13bb7dde20290d3615db5
Ahmed Saladin <as.yuri74@gmail.com>
2021-06-07 22:54:54 +0200

    getting data from api

--- Frontend/src/app/components/edit-tailor-profile/edit-tailor-profile.component.ts
+++ Frontend/src/app/components/edit-tailor-profile/edit-tailor-profile.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { TailorService } from 'src/app/services/tailor.service';
 
 @Component({
   selector: 'app-edit-tailor-profile',
   templateUrl: './edit-tailor-profile.component.html',
-  styleUrls: ['./edit-tailor-profile.component.css']
+  styleUrls: ['./edit-tailor-profile.component.css'],
 })
 export class EditTailorProfileComponent implements OnInit {
+  constructor(private api: TailorService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  user: any = {
+    name: 'kiki',
+    about: '3la allah 7kaytk',
+    avatar: '',
+    gallary: ['', '', ''],
+  };
+  information() {
+    this.api.get_tailor_info(2).subscribe(
+      (res) => {
+        console.log(res);
+        this.user = res.body;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
-
 }
--- Frontend/src/app/services/tailor.service.ts
+++ Frontend/src/app/services/tailor.service.ts
@@ -7,8 +7,11 @@ import { Injectable } from '@angular/core';
 export class TailorService {
   private url = 'http://localhost:3000/users';
   constructor(private http: HttpClient) {}
-  get_tailor_info() {
+  get_tailors_info() {
     return this.http.get(this.url, { observe: 'response' });
+  }
+  get_tailor_info(id: any) {
+    return this.http.get(`${this.url}/${id}`, { observe: 'response' });
   }
   update_tailor_info(id: any, body: any) {
     return this.http.put(`${this.url}/${id}`, body, { observe: 'response' });
