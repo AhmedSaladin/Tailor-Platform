a1e5016342939ee0018704071706e353454c8de1
Omar Salama <osalama@Omars-MacBook-Pro.local>
2021-06-08 01:39:45 +0200

    add home component

---
+++ Frontend/src/app/components/tailors/tailors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tailors',
+  templateUrl: './tailors.component.html',
+  styleUrls: ['./tailors.component.css']
+})
+export class TailorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
