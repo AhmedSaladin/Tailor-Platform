9e6ed1696d8f77a94891bf04fa36569dc623d55f
Ahmed Saladin <as.yuri74@gmail.com>
2021-06-07 21:11:41 +0200

    adding tailor services

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditTailorProfileComponent } from './components/edit-tailor-profile/edit-tailor-profile.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EditTailorProfileComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EditTailorProfileComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Frontend/src/app/services/tailor.service.ts
+++ Frontend/src/app/services/tailor.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TailorService {
-
-  constructor() { }
+  private url = 'http://localhost:3000/users';
+  constructor(private http: HttpClient) {}
+  get_tailor_info() {
+    return this.http.get(this.url, { observe: 'response' });
+  }
+  update_tailor_info(id: any, body: any) {
+    return this.http.put(`${this.url}/${id}`, body, { observe: 'response' });
+  }
 }
