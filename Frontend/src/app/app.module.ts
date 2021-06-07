cb36ed91bb26e3cd3161d69e751c33b102d40ec2
Ahmed Saladin <as.yuri74@gmail.com>
2021-06-07 23:04:49 +0200

    adding footer component

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditTailorProfileComponent } from './components/edit-tailor-profile/edit-tailor-profile.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, EditTailorProfileComponent, NavBarComponent],
+  declarations: [AppComponent, EditTailorProfileComponent, NavBarComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
