cd306b6438658f65f66192793eeab873d72c6014
Ahmed Saladin <as.yuri74@gmail.com>
2021-06-07 20:48:14 +0200

    adding edit tailor-profile

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditTailorProfileComponent } from './components/edit-tailor-profile/edit-tailor-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditTailorProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/src/app/components/edit-tailor-profile/edit-tailor-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-tailor-profile',
+  templateUrl: './edit-tailor-profile.component.html',
+  styleUrls: ['./edit-tailor-profile.component.css']
+})
+export class EditTailorProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
