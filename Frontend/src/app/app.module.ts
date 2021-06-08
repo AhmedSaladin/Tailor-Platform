eb71b9232a265c8c6cbb624c75a926f318eb5f32
Omar Salama <omarzalama@gmail.com>
2021-06-09 01:00:52 +0200

    restucturing home component related folders

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { EditTailorProfileComponent } from './components/edit-tailor-profile/edit-tailor-profile.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutTailorComponent } from './components/home/about-tailor/about-tailor.component';
 
 @NgModule({
-  declarations: [AppComponent, EditTailorProfileComponent, NavBarComponent, FooterComponent],
+  declarations: [AppComponent, EditTailorProfileComponent, NavBarComponent, FooterComponent, HomeComponent, AboutTailorComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
