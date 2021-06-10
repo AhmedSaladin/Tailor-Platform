14b831b05918d180ab6a6e0e5d26d6387579284f
Mahmoud SabahAllah <mahmoud.sabahallah@gmail.com>
2021-06-10 16:02:07 +0200

    flag

--- Frontend/src/app/components/registration/registration.component.ts
+++ Frontend/src/app/components/registration/registration.component.ts
@@ -14,7 +14,7 @@ export class RegistrationComponent implements OnInit {
   }
 
   AddCustomer(fname: any, lname: any, email: any, password: any) {
-    let isTailor: any;
+    let isTailor: false;
     let customer = { fname: fname, lname: lname, email: email, password: password, IsTailor: false }
     this.myCustomer.AddNewCustomer(customer).subscribe();
   }
