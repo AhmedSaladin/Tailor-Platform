1cbfc2c451b6ebb17668169a69cef3999606b3d5
Ahmed Saladin <as.yuri74@gmail.com>
2021-06-11 11:47:34 +0200

    adding unsubscribe function

--- Frontend/src/app/components/Tailor/tailor-about/tailor-about.component.ts
+++ Frontend/src/app/components/Tailor/tailor-about/tailor-about.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { TailorService } from 'src/app/services/tailor.service';
 
 @Component({
@@ -6,14 +6,20 @@ import { TailorService } from 'src/app/services/tailor.service';
   templateUrl: './tailor-about.component.html',
   styleUrls: ['./tailor-about.component.css'],
 })
-export class TailorAboutComponent implements OnInit {
+export class TailorAboutComponent implements OnInit, OnDestroy {
   @Input() user_info: any;
+  eve: any;
   constructor(private api: TailorService) {}
 
   ngOnInit(): void {}
 
   update_tailor_about_section(about: string) {
     this.user_info.about = about;
-    this.api.update_tailor_info(this.user_info.id, this.user_info).subscribe();
+    this.eve = this.api
+      .update_tailor_info(this.user_info.id, this.user_info)
+      .subscribe();
+  }
+  ngOnDestroy(): void {
+    this.eve.unsubscribe();
   }
 }
