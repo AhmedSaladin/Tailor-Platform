754c2ccb1ee927a1dfe3cd4f2a7fa2b07b41f6dc
Ahmed Saladin <36247818+AhmedSaladin@users.noreply.github.com>
2021-06-11 15:35:04 +0200

    return to stable state

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { EditTailorProfileComponent } from './components/Tailor/edit-tailor-profile/edit-tailor-profile.component';
 
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // TODO Change it with the landing-page when it's done
+  { path: 'profile/edit/:id', component: EditTailorProfileComponent },
 ];
 
 @NgModule({
