640378078bf1c61949eec58ec7acba1115634819
Omar Salama <omarzalama@gmail.com>
2021-06-13 18:04:14 +0200

    initial filtering stage

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { TailorService } from 'src/app/services/tailor.service';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  filteredTailors: string[] = new Array();
+  tempFilter: any;
   constructor(private tailorInfo: TailorService) {}
   tailors: any;
+  selectedItems: string[] = new Array();
   ngOnInit(): void {
     this.tailorInfo.get_tailors_info().subscribe(
       (res) => {
@@ -18,6 +20,23 @@ export class HomeComponent implements OnInit {
       (err) => {
         console.log(err);
       }
-    )
+    );
+  }
+
+  filterTailors(e: any) {
+    if (e.target.checked) {
+      this.tempFilter = this.tailors.filter(
+        (tailor: any) => tailor.designFor === e.target.value
+      );
+      // this.filteredTailors.push(this.tempFilter); //temp array of object
+      this.selectedItems.push(e.target.id);
+    } else {
+      this.selectedItems = this.selectedItems.filter(elm => elm != e.target.id)
+    }
+    console.log(this.tempFilter);
+    // console.log(this.selectedItems)
+  }
+  isFiltered() {
+    return  !(this.selectedItems.length === 0);
   }
 }
