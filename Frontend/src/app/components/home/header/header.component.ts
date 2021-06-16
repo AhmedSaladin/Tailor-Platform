ad4c5953af3e383316376f2973e0ac46f7eea797
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-17 01:51:24 +0200

    pass the name to getTailorInfoByName server-function

--- Frontend/src/app/components/home/header/header.component.ts
+++ Frontend/src/app/components/home/header/header.component.ts
@@ -8,16 +8,19 @@ import { TailorService } from 'src/app/services/tailor.service';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  name: string = '';
   constructor(
     private tailorService: TailorService,
     private MyActived: ActivatedRoute,
     private router: Router
-  ) {}
+  ) {
+    this.name = MyActived.snapshot.params.user;
+  }
   tailors: any;
   ngOnInit(): void {
-    this.tailorService.get_tailor_info_by_name('').subscribe(
+    this.tailorService.get_tailor_info_by_name(this.name).subscribe(
       (res) => {
-        this.tailors = res;
+        this.tailors = res.body;
       },
       (err) => {
         console.log(err);
