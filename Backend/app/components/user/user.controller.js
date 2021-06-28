c088ef9fb8df1ebcac7512598e9aa6c5d9120763
Ahmed Saladin <36247818+AhmedSaladin@users.noreply.github.com>
2021-06-28 23:19:24 +0200

    adding user routes and update controller

--- Backend/app/components/user/user.controller.js
+++ Backend/app/components/user/user.controller.js
@@ -3,26 +3,24 @@ const mongoID = require("mongoose");
 const { check_password, hashing } = require("../../utility/password");
 const INTERNAL_SERVER_ERROR = 500;
 
-exports = {
+module.exports = {
   get_user: async (req, res, next) => {
     try {
+      console.log(req.params.id);
       const id = req.params.id;
-      if (!mongoID.isValidObjectId(id))
-        throw Error({ status: 400, message: "Invalid ID" });
+      if (!mongoID.isValidObjectId(id)) res.status(400).json();
       const user = await User.findById(id);
       res.status(200).json(user);
-    } catch (err) {
-      throw Error({ status: INTERNAL_SERVER_ERROR, message: err.toString() });
-    }
+    } catch (err) {}
   },
 
   sign_up: async (req, res, next) => {
     try {
+      
       const user = req.body;
       const email = user.email;
       const is_found = await User.findOne({ email });
-      if (is_found == [])
-        throw Error({ status: 406, message: "Email registered already" });
+      if (is_found == []) res.status(406).json();
       const password = await hashing(user.password);
       const new_user = new User({
         name: user.name,
