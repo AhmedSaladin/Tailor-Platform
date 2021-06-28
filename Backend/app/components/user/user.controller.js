a848a447bce0a2aadd5db6083223b530bef7e28e
Ahmed Saladin <36247818+AhmedSaladin@users.noreply.github.com>
2021-06-28 21:40:25 +0200

    adding user logic

---
+++ Backend/app/components/user/user.controller.js
@@ -0,0 +1,53 @@
+const User = require("./user.model");
+const mongoID = require("mongoose");
+const { check_password, hashing } = require("../../utility/password");
+const INTERNAL_SERVER_ERROR = 500;
+
+exports = {
+  get_user: async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      if (!mongoID.isValidObjectId(id))
+        throw Error({ status: 400, message: "Invalid ID" });
+      const user = await User.findById(id);
+      res.status(200).json(user);
+    } catch (err) {
+      throw Error({ status: INTERNAL_SERVER_ERROR, message: err.toString() });
+    }
+  },
+
+  sign_up: async (req, res, next) => {
+    try {
+      const user = req.body;
+      const email = user.email;
+      const is_found = await User.findOne({ email });
+      if (is_found == [])
+        throw Error({ status: 406, message: "Email registered already" });
+      const password = await hashing(user.password);
+      const new_user = new User({
+        name: user.name,
+        email: user.email,
+        password,
+      });
+      new_user.save();
+      console.log(new_user);
+      res.status(201).json();
+    } catch (err) {
+      throw Error({ status: INTERNAL_SERVER_ERROR, message: err.toString() });
+    }
+  },
+
+  login: async (req, res, next) => {
+    try {
+      const { email } = req.body;
+      const { password } = req.body;
+      const found = await User.findOne({ email });
+      if (found == []) throw Error({ status: 404, message: "Email not found" });
+      const valid_password = await check_password(password, found.password);
+      if (!valid_password) res.status(400).json();
+      res.status(200).json();
+    } catch (err) {
+      throw Error({ status: INTERNAL_SERVER_ERROR, message: err.toString() });
+    }
+  },
+};
