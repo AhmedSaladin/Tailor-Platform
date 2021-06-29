50d0b3c04a16df68e4b9ba0f6600967386235e77
Ahmed Saladin <36247818+AhmedSaladin@users.noreply.github.com>
2021-06-29 03:46:40 +0200

    add promise wrapper and error handler

--- Backend/app/components/user/user.controller.js
+++ Backend/app/components/user/user.controller.js
@@ -2,21 +2,20 @@ const User = require("./user.model");
 const mongoID = require("mongoose");
 const { check_password, hashing } = require("../../utility/password");
 const INTERNAL_SERVER_ERROR = 500;
+const promise_handler = require("../../utility/promiseHandler");
 
 module.exports = {
   get_user: async (req, res, next) => {
-    try {
-      console.log(req.params.id);
-      const id = req.params.id;
-      if (!mongoID.isValidObjectId(id)) res.status(400).json();
-      const user = await User.findById(id);
-      res.status(200).json(user);
-    } catch (err) {}
+    const id = req.params.id;
+    if (!mongoID.isValidObjectId(id))
+      next({ status: 400, message: "Not valid ID" });
+    const [user, err] = await promise_handler(User.findById(id));
+    if (user) return res.status(200).json(user);
+    next({ status: 400, message: err.toString() });
   },
 
   sign_up: async (req, res, next) => {
     try {
-      
       const user = req.body;
       const email = user.email;
       const is_found = await User.findOne({ email });
