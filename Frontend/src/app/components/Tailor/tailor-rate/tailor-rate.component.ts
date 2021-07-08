71c2b1cdb81892b79767949eb125836a3f71ea10
Ahmed Saladin <as.yuri74@gmail.com>
2021-07-08 18:14:46 +0200

    adding comment component

--- Frontend/src/app/components/Tailor/tailor-rate/tailor-rate.component.ts
+++ Frontend/src/app/components/Tailor/tailor-rate/tailor-rate.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommentService } from 'src/app/services/comment.service';
 
 @Component({
   selector: 'app-tailor-rate',
   templateUrl: './tailor-rate.component.html',
-  styleUrls: ['./tailor-rate.component.css']
+  styleUrls: ['./tailor-rate.component.css'],
 })
 export class TailorRateComponent implements OnInit {
-
-  constructor() { }
+  @Input() comments: any;
+  constructor(private api: CommentService) {}
 
   ngOnInit(): void {
+    this.get_comments();
+    console.log(this.comments);
   }
-
+  get_comments() {
+    let arr: [];
+    this.comments.map((id: string) => {
+      this.api.get_singel_comment(id).subscribe(
+        (res) => {
+          // arr.push(res.body);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    });
+  }
 }
