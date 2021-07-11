88b0dd513f726779f32aa4116ff687a749a76c1e
Omar Salama <31420520+omar-salama@users.noreply.github.com>
2021-07-12 00:21:36 +0200

    updating password regex

--- Frontend/src/app/components/registration/registration.component.ts
+++ Frontend/src/app/components/registration/registration.component.ts
@@ -54,7 +54,7 @@ export class RegistrationComponent implements OnInit {
         [
           Validators.required,
           Validators.pattern(
-            '^.*(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()?_ \'\"\-+=]).*$'
+            '/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/'
           ),
         ],
       ],
