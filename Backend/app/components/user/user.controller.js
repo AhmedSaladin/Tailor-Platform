a65a82ae6df7fd630708baaa4ebd8c210f60283b
Omar Salama <31420520+omar-salama@users.noreply.github.com>
2021-07-11 03:12:39 +0200

    user controller refactoring

--- Backend/app/components/user/user.controller.js
+++ Backend/app/components/user/user.controller.js
@@ -3,7 +3,6 @@ const mongoID = require("mongoose");
 const { check_password, hashing } = require("../../utility/password");
 const INTERNAL_SERVER_ERROR = 500;
 const promise_handler = require("../../utility/promiseHandler");
-const errorHandler = require("../../middlewares/errorHandler");
 
 module.exports = {
   get_user: async (req, res, next) => {
@@ -16,33 +15,31 @@ module.exports = {
   },
 
   sign_up: async (req, res, next) => {
+    const { name, email, phone, password } = req.body;
     try {
-      const user = req.body;
-      const email = user.email;
-      const is_found = await User.findOne({ email });
-      if (is_found) throw { status: 406, message: "Email already exists." };
-      const password = await hashing(user.password);
-      const new_user = new User({
-        name: user.name,
-        email: user.email,
-        phone: user.phone,
-        password,
+      const found = await User.findOne({ email });
+      if (found) throw { status: 403, message: "Email already exists." };
+      const hashed_password = await hashing(password);
+      const user = await User.create({
+        name,
+        email,
+        phone,
+        password: hashed_password,
       });
-      new_user.save();
-      res.status(201).json();
+      res.status(201).json(user);
     } catch (err) {
       next(err);
     }
   },
 
   login: async (req, res, next) => {
+    const { email, password } = req.body;
     try {
-      const { email } = req.body;
-      const { password } = req.body;
       const found = await User.findOne({ email });
-      if (!found) throw { status: 404, message: "Wrong email or password." };
+      if (!found) throw { status: 403, message: "Wrong email or password." };
       const valid_password = await check_password(password, found.password);
-      if (!valid_password) throw { status: 404, message: "Wrong email or password." };
+      if (!valid_password)
+        throw { status: 403, message: "Wrong email or password." };
       res.status(200).json();
     } catch (err) {
       next(err);
