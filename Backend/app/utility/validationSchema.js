4f85d74cf515b78ce90025d043b151169b05b9ef
Omar Salama <31420520+omar-salama@users.noreply.github.com>
2021-07-12 23:47:50 +0200

    implementing jwt

--- Backend/app/utility/validationSchema.js
+++ Backend/app/utility/validationSchema.js
@@ -1,16 +1,19 @@
 const Joi = require("joi");
 
-module.exports = { userSchema: Joi.object({
+module.exports = {
+  userSchema: Joi.object({
     name: Joi.string().min(3).max(30).required(),
     email: Joi.string().email().lowercase().required(),
-    phone: Joi.number().min(11).required(),
+    phone: Joi.string()
+      .required()
+      .pattern(new RegExp("^(010|011|012|015)[0-9]{8}$")),
     password: Joi.string()
       .min(8)
       .required()
       .pattern(
         new RegExp(
-          "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$"
+          "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*()-_]).{8,}$"
         )
       ),
-  })
-}
+  }),
+};
