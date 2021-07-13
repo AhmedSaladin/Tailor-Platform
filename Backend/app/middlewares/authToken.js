55285b1578418e54393fcd14cdb6ae0550c26236
Ahmed Saladin <as.yuri74@gmail.com>
2021-07-13 12:11:46 +0200

    adding http status code

--- Backend/app/middlewares/authToken.js
+++ Backend/app/middlewares/authToken.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const { FORBIDDEN, UNAUTHORIZED } = require("../utility/statusCodes");
 
 const authToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   // getting token from -> Bearer TOKEN
   const token = authHeader && authHeader.split(" ")[1];
-  if (!token) return res.sendStatus(401);
+  if (!token) return res.sendStatus(UNAUTHORIZED);
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decodedToken) => {
-    if (err) res.sendStatus(403);
+    if (err) return res.sendStatus(FORBIDDEN);
     req.user = decodedToken;
     next();
   });
