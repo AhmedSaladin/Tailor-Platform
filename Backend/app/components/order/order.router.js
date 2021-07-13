320eb854d5fcf85ce216f43f7a49df8b15423e50
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-07-13 13:14:15 +0200

    create new orders

--- Backend/app/components/order/order.router.js
+++ Backend/app/components/order/order.router.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const { Mongoose } = require('mongoose');
+const orderModel = require('./order.model');
 const router = express.Router();
 
 
-router.get('/',(req , res)=>{});
-router.post('/',(req , res)=>{});
+router.post('/',(req , res , next )=>{
+    const order = new orderModel({
+        _id: Mongoose.Types.ObjectId(),
+        customerID: req.body.customerID,
+        tailorID: req.body.tailorID,
+        designs: req.body.designs,
+        sizes: req.body.sizes, 
+    });
+    order.save().then(result =>{
+        console.log(result);
+        res.status(201).json(result);
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    });
+});
 
 module.exports = router;
