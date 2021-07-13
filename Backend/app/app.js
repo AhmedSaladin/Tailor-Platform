23225aee09aeb2e259df507d17daee9f1452bb2d
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-07-14 01:23:30 +0200

    view order by tailor id and customer id

--- Backend/app/app.js
+++ Backend/app/app.js
@@ -14,8 +14,8 @@ module.exports = (app) => {
   app.use(express.json());
   app.use("/api/users", User);
   app.use("/api/comments", commentRouters);
-  app.use(err);
   app.use('/api/orders',order);
+  app.use(err);
   app.use(errorHandler);
   app.use("*", notFound);
 };
