972e62096b56c6b4caa27c1c7d3b627d79499275
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-07-17 17:50:01 +0200

    router order

--- Backend/app/components/order/order.router.js
+++ Backend/app/components/order/order.router.js
@@ -5,7 +5,7 @@ const orderController = require('./order.controller')
 
 
 router.post('/',orderController.create_order);
-router.get('/',orderController.view_order);
+router.get('/orders',orderController.view_order);
 router.get('/customer-order',orderController.view_orderByCustomer);
 router.get('/order',orderController.view_orderByOrder);
 router.get('/tailor-order',orderController.view_orderByTailor);
