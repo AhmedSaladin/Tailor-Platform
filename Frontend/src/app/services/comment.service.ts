8b854fbaa7f8ee5c3ccac28cc5c1e54d04235ec3
nancy <nany.moris44@gmail.com>
2021-07-18 13:46:57 +0200

    count of undefined

--- Frontend/src/app/services/comment.service.ts
+++ Frontend/src/app/services/comment.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CommentService {
-  private url = 'http://localhost:3001/api/comments';
-  private BaseUrl = 'http://localhost:3000/comments';
+  private BaseUrl = 'http://localhost:3001/api/comments';
+  private url = 'http://localhost:3000/comments';
 
   constructor(private http: HttpClient) {}
   get_single_comment(id: string) {
