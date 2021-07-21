567c8d5a42d18ff30d03d36cd437836a715689b3
Ahmed Saladin <as.yuri74@gmail.com>
2021-07-21 12:21:34 +0200

    updating tailors dashboard

--- Frontend/src/app/components/dashboard/tailors-dashboard/tailors-dashboard.component.ts
+++ Frontend/src/app/components/dashboard/tailors-dashboard/tailors-dashboard.component.ts
@@ -1,30 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, NgForm, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
 import { TailorService } from 'src/app/services/tailor.service';
+import { Tailor } from '../../shared/models';
 
 @Component({
   selector: 'app-tailors-dashboard',
   templateUrl: './tailors-dashboard.component.html',
-  styleUrls: ['./tailors-dashboard.component.css']
+  styleUrls: ['./tailors-dashboard.component.css'],
 })
-export class TailorsDashboardComponant implements OnInit {
+export class TailorsDashboardComponant implements OnInit, OnDestroy {
   tailor: any;
-  tailors: any;
+  tailors!: Array<Tailor>;
   isTailor = true;
   id: any;
   formValidation: any;
+  eve!: Subscription;
 
-  constructor(private tailorServive: TailorService , private formBuilder: FormBuilder) {
-    // this.id.params.id;
-  }
+  constructor(
+    private tailorServive: TailorService,
+    private formBuilder: FormBuilder,
+    private tostr: ToastrService
+  ) {}
 
   ngOnInit(): void {
-    this.tailorServive.getTailorsInfo().subscribe(
-      (res) => { this.tailors = res },
-      (err) => { console.log(err) }
-    )
-
+    this.get_tailors();
     this.formValidation = this.formBuilder.group({
       fname: [
         '',
@@ -42,7 +43,7 @@ export class TailorsDashboardComponant implements OnInit {
           Validators.pattern('[A-Z-a-z0-9_-]{3,10}$'),
         ],
       ],
-      phone:[
+      phone: [
         '',
         [
           Validators.required,
@@ -51,12 +52,8 @@ export class TailorsDashboardComponant implements OnInit {
           Validators.pattern('^(010|011|012|015)[0-9]{8}$'),
         ],
       ],
-      gender:['',
-        Validators.required
-      ],
-      designFor:[ '',
-        Validators.required
-      ],
+      gender: ['', Validators.required],
+      designFor: ['', Validators.required],
       email: [
         '',
         [
@@ -81,9 +78,8 @@ export class TailorsDashboardComponant implements OnInit {
     return this.formValidation.controls;
   }
 
-
   AddTailor(form: NgForm) {
-    console.log(form)
+    console.log(form.value);
     let tailor = {
       name: form.value.fname + ' ' + form.value.lname,
       phone: form.value.phone,
@@ -92,23 +88,42 @@ export class TailorsDashboardComponant implements OnInit {
       email: form.value.email,
       password: form.value.password,
     };
-    this.tailorServive.AddNewTailor(tailor).subscribe();
+    this.tailorServive.AddNewTailor(tailor).subscribe(
+      () => this.tostr.success('Tailor added successfully'),
+      (err) => this.tostr.error(err)
+    );
     form.reset();
-  
   }
+
   getTailor(id: any) {
-    console.log(id)
-    return this.tailorServive.get_tailor_info(id).subscribe(
-      (res => this.tailor = res.body),
-      (err => console.log(err))
+    this.tailorServive.get_tailor_info(id).subscribe(
+      (res) => (this.tailor = res.body),
+      (err) => this.tostr.error(err)
     );
   }
 
   deleteTailor(id: any) {
-    return this.tailorServive.deleteTailor(id).subscribe(
-      (res => console.log(res)),
-      (err => console.log(err))
-    )
+    this.tailorServive.deleteTailor(id).subscribe(
+      () => {
+        this.tostr.success('Tailor deleted successfully');
+        this.tailors = this.tailors.filter((tailor) => tailor._id != id);
+      },
+      (err) => this.tostr.error(err)
+    );
+  }
+
+  get_tailors() {
+    this.eve = this.tailorServive.get_tailors_info().subscribe(
+      (res) => {
+        if (res.body == null) this.tailors = [];
+        else this.tailors = res.body;
+      },
+      (err) => {
+        this.tostr.error(err);
+      }
+    );
+  }
+  ngOnDestroy(): void {
+    if (this.eve != undefined) this.eve.unsubscribe();
   }
 }
-
--- Frontend/src/app/services/tailor.service.ts
+++ Frontend/src/app/services/tailor.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, take, tap } from 'rxjs/operators';
+import { Tailor } from '../components/shared/models';
 import { BindingService } from './binding/binding.service';
 
 @Injectable({
@@ -11,40 +12,51 @@ export class TailorService {
   private url = 'http://localhost:3000/api/tailors';
   constructor(private http: HttpClient, private binding: BindingService) {}
   get_tailors_info() {
-    return this.http.get(this.url, { observe: 'response' });
-  }
-  get_tailors_info_filter(filter: any) {
-    return this.http.get(`${this.url}?${filter}`, { observe: 'response' });
-  }
-  get_tailor_info(id: any) {
     this.binding.changeLoading(true);
-    return this.http.get(`${this.url}/${id}`, { observe: 'response' }).pipe(
+    return this.http.get<Array<Tailor>>(this.url, { observe: 'response' }).pipe(
       catchError(this.handleError),
       tap(() => this.binding.changeLoading(false))
     );
   }
+
+  get_tailors_info_filter(filter: any) {
+    return this.http.get(`${this.url}?${filter}`, { observe: 'response' });
+  }
+
+  get_tailor_info(id: any) {
+    this.binding.changeLoading(true);
+    return this.http.get(`${this.url}/${id}`, { observe: 'response' }).pipe(
+      catchError(this.handleError),
+      tap(() => this.binding.changeLoading(false)),
+      take(1)
+    );
+  }
+
   update_tailor_info(id: any, body: any) {
-    return this.http.patch(`${this.url}/${id}`, body, { observe: 'response' });
-  }
-  AddNewTailor(tailor: any) {
-    return this.http.post(this.url, tailor);
-  }
-  deleteTailor(id: any) {
-    return this.http.delete(`${this.url}/${id}`, { observe: 'response' });
-  }
-  getTailorsInfo() {
     this.binding.changeLoading(true);
     return this.http
-      .get(this.url)
-      .pipe(tap((res) => this.binding.changeLoading(false)));
+      .patch(`${this.url}/${id}`, body, { observe: 'response' })
+      .pipe(
+        catchError(this.handleError),
+        tap(() => this.binding.changeLoading(false))
+      );
   }
+
+  AddNewTailor(tailor: any) {
+    return this.http
+      .post(this.url, tailor)
+      .pipe(catchError(this.handleError), take(1));
+  }
+
+  deleteTailor(id: any) {
+    return this.http
+      .delete(`${this.url}/${id}`, { observe: 'response' })
+      .pipe(catchError(this.handleError), take(1));
+  }
+
   private handleError(err: HttpErrorResponse) {
     if (err.error.message == 'Invalid ID.')
       return throwError('Tailor not found');
-    // if (err.error.message == 'NOT FOUND')
-    //   return throwError('Email or Password wrong.');
-
-    // if (!err.error.message) return throwError('Somthing went wrong.');
     return throwError(err.error.message);
   }
 }
