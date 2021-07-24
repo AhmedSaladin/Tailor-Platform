d99f6563c1974b1c0418f80e4c271b62f6785063
Omar Salama <31420520+omar-salama@users.noreply.github.com>
2021-07-25 01:16:19 +0200

    deleting unccessary comments and general clean up

--- Frontend/src/app/components/home/about-tailor/about-tailor.component.ts
+++ Frontend/src/app/components/home/about-tailor/about-tailor.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { CommentService } from 'src/app/services/comment.service';
 
 @Component({
@@ -7,46 +8,43 @@ import { CommentService } from 'src/app/services/comment.service';
   templateUrl: './about-tailor.component.html',
   styleUrls: ['./about-tailor.component.css'],
 })
-export class AboutTailorComponent implements OnInit {
-  constructor(private router: Router,private apiComment:CommentService) {}
+export class AboutTailorComponent implements OnInit, OnDestroy {
+  constructor(private router: Router, private apiComment: CommentService) {}
   goToTailorProfile(id: any) {
     this.router.navigateByUrl(`/tailor/${id}`);
   }
   ngOnInit(): void {
-    this.get_tailor_rate()
+    this.get_tailor_rate();
   }
   @Input() tailor: any;
-  eve:any;
+  eve!: Subscription;
   review: any;
   starsTotal = 5;
   myStyle: any = {};
 
+  get_tailor_rate() {
+    this.eve = this.apiComment
+      .getTailorRate(this.tailor._id)
+      .subscribe((res) => {
+        this.review = res.body;
+        if (this.review.length == 0) {
+          this.review[0] = {
+            count: 0,
+            rate: 5,
+          };
+        }
+        this.review = this.review[0];
+        this.displayRate(this.review.rate);
+      });
+  }
 
-   ///////////////////////get tailor rate//////////////////////////////////
-   get_tailor_rate() {
-     this.eve = this.apiComment
-       .getTailorRate(this.tailor._id)
-       .subscribe((res) => {
-         console.log(res.body)
-         this.review = res.body;
-         if (this.review.length == 0) {
-           this.review[0] = {
-             count: 0,
-             rate: 5,
-           };
-           console.log(this.review);
-         }
-         this.review = this.review[0];
-         this.displayRate(this.review.rate);
-       });
-   }
-
-   ///////////////displayRate//////////////////////
-   displayRate(rate: any) {
-     const starPercentage = (rate / 5) * 100;
-     // Round to nearest 10
-     const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
-     this.myStyle = { width: starPercentageRounded };
-     console.log(this.myStyle);
-    }
+  displayRate(rate: any) {
+    const starPercentage = (rate / 5) * 100;
+    // Round to nearest 10
+    const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+    this.myStyle = { width: starPercentageRounded };
+  }
+  ngOnDestroy(): void {
+    if (this.eve) this.eve.unsubscribe();
+  }
 }
